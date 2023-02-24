77e8cc4a911cf352a7d9fa523756de30a4eaeb41
Brian Reardon <Brian.D.Reardon@gmail.com>
2023-02-24 09:49:48 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name){
+
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+
+    cats.unshift(name);
+}
+
+function destructivelyRemoveFirstCat(){
+
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat(){
+
+    cats.pop();
+}
+
+function appendCat(name){
+
+    const newCats = [...cats, name];
+    return newCats;
+}
+
+function prependCat(name){
+
+    const newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat(){
+
+    const newCats = cats.slice(0, 2);
+
+    return newCats;
+}
+
+function removeFirstCat(){
+
+    const newCats = cats.slice(1,3);
+
+    return newCats;
+}
+
+removeLastCat();
